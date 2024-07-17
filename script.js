cf7c55aa80757be4a93e7431077cc4361dc141ef
ernestngenest <ernestprabowo@gmail.com>
2024-07-17 16:06:30 +0700

    logiccommit

---
+++ script.js
@@ -0,0 +1,6 @@
+let db = {};
+let konversi = document.getElementById('konversi-btn')
+document.getElementById('coin-btn').addEventListener('click', function() {
+    document.getElementById('coin').value
+    document.getElementById('amount').value = document.getElementById('coin').value
+});
