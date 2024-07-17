069bfb80313ee2427af0076dda1689854aa63120
ernestngenest <ernestprabowo@gmail.com>
2024-07-17 16:55:04 +0700

    script update

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-let db = {};
-let konversi = document.getElementById('konversi-btn')
-document.getElementById('coin-btn').addEventListener('click', function() {
-    document.getElementById('coin').value
-    document.getElementById('amount').value = document.getElementById('coin').value
+// let db = {};
+// let konversi = document.getElementById('konversi-btn')
+// document.getElementById('coin-btn').addEventListener('click', function() {
+//     document.getElementById('coin').value
+//     document.getElementById('amount').value = document.getElementById('coin').value
+// });
+
+const factor = 14000;
+
+const usd = document.getElementById("usd");
+const idr = document.getElementById("idr");
+
+usd.addEventListener("input", function(ev) {
+    const value = ev.currentTarget.value;
+    const valueIDR = value * factor;
+    idr.value = valueIDR;
+});
+
+idr.addEventListener("input", function(ev) {
+    const value = ev.currentTarget.value;
+    const valueUSD = value / factor;
+    usd.value = valueUSD;
 });
